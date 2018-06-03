dc171d74655b3505fd26a6d7efb990e0f9d88f2e
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-06-03 21:06:02 +0900

    fix: fix template of config_test.go

--- usecase/cfgtestgo_template.go
+++ usecase/cfgtestgo_template.go
@@ -3,28 +3,21 @@ package usecase
 // DefaultCfgTestTmpl is the default template of the generated wrapper test code.
 const DefaultCfgTestTmpl = `
 {{ $paramUC := .ParamUC -}}
-{{ $pkgName := .CfgUC.GetPkgName .Cfg -}}
-package {{$pkgName}}_test
+package {{.CfgUC.GetPkgName .Cfg}}
 
 import (
 	"testing"
-	{{- if .CfgUC.GetPkg .Cfg}}
-
-	"{{.CfgUC.GetPkg .Cfg}}"
-	{{- else}}
-	// import your cfg package
-	{{ end -}}
 )
 
 {{- range .Cfg.Params}}
 func Test{{$paramUC.CamelCaseName .}}(t *testing.T) {
 	// write your test
-	{{$pkgName}}.Get{{$paramUC.CamelCaseName .}}()
+	Get{{$paramUC.CamelCaseName .}}()
 }
 
 func TestSet{{$paramUC.CamelCaseName .}}(t *testing.T) {
 	// write your test
-	{{$pkgName}}.Set{{$paramUC.CamelCaseName .}}({{ $paramUC.GetDefaultStr .}})
+	Set{{$paramUC.CamelCaseName .}}({{ $paramUC.GetDefaultStr .}})
 }
 {{- end}}
 `
