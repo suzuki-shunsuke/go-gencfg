46450cebd996e4adc530ff56269564061b708f42
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-12-13 22:38:00 +0900

    refactor: use go-cliutil

--- internal/handler/gen.go
+++ internal/handler/gen.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"github.com/suzuki-shunsuke/go-cliutil"
 	"github.com/urfave/cli"
 
 	"github.com/suzuki-shunsuke/go-gencfg/internal/config"
@@ -11,22 +12,19 @@ import (
 
 // CmdGen is sub command "gen".
 func CmdGen(c *cli.Context) error {
-	err := usecase.GenCfgFile(
-		domain.GenCfgFileArgs{
-			Src:       config.GetCfgPath(c),
-			Dest:      config.GetDestPath(c),
-			TmplPath:  config.GetTemplatePath(c),
-			Reader:    registry.NewFileReader(),
-			CfgReader: registry.NewCfgReader(),
-			Generator: registry.NewCodeGenerator(),
-			Executer:  registry.NewCmdExecuter(),
-			CfgUC:     registry.NewCfgUsecase(),
-			EnvUC:     registry.NewEnvUsecase(),
-			FlagUC:    registry.NewFlagUsecase(),
-			ParamUC:   registry.NewParamUsecase(),
-		})
-	if err == nil {
-		return nil
-	}
-	return cli.NewExitError(err.Error(), 1)
+	return cliutil.ConvErrToExitError(
+		usecase.GenCfgFile(
+			domain.GenCfgFileArgs{
+				Src:       config.GetCfgPath(c),
+				Dest:      config.GetDestPath(c),
+				TmplPath:  config.GetTemplatePath(c),
+				Reader:    registry.NewFileReader(),
+				CfgReader: registry.NewCfgReader(),
+				Generator: registry.NewCodeGenerator(),
+				Executer:  registry.NewCmdExecuter(),
+				CfgUC:     registry.NewCfgUsecase(),
+				EnvUC:     registry.NewEnvUsecase(),
+				FlagUC:    registry.NewFlagUsecase(),
+				ParamUC:   registry.NewParamUsecase(),
+			}))
 }
